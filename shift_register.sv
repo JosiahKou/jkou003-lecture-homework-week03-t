445571c5d2abadf4b6bbb84ec0de570fe5ea5126
JosiahKou <155879792+JosiahKou@users.noreply.github.com>
2026-04-14 17:41:25 +0000

    fix

--- shift_register.sv
+++ shift_register.sv
@@ -1,25 +1,23 @@
 module shift_register #(parameter DIRECTION = 1) (
-    input logic clk,
-    input logic rst,
-    input logic data,
-    input logic wr_en,
-    output logic [31:0] out
+    input clk,
+    input rst,
+    input data,
+    input wr_en,
+    output [31:0] out
 );
 
-logic [31:0] reg_value;
+reg [31:0] out;
 
-always_ff @(posedge clk) begin
+always @(posedge clk) begin
     if (rst) begin
-        reg_value <= 32'b0;
+        out <= 32'b0;
     end else if (wr_en) begin
         if (DIRECTION == 1) begin
-            reg_value <= {data, reg_value[31:1]};
+            out <= {data, out[31:1]};
         end else begin
-            reg_value <= {reg_value[30:0], data};
+            out <= {out[30:0], data};
         end
     end
 end
-
-assign out = reg_value;
 
 endmodule
